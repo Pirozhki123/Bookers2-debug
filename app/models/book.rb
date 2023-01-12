ffa42f9ccddb1b434af55320bb7e94e3fad7d9d0
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-12 21:47:02 +0000

    [start]課題3【Bookers2へ「いいね/コメント機能」を実装】

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :favorite, dependent: :destroy
   validates :title,presence: true
   validates :body,presence: true,length:{maximum:200}
 end

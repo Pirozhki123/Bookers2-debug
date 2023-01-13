444cf5b27885d0e5ad84b39dd83f4aa37c1a9f51
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-13 04:24:45 +0000

    [add]book_commentモデル・コントローラー,view

---
+++ db/migrate/20230113040516_create_book_comments.rb
@@ -0,0 +1,10 @@
+class CreateBookComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :book_comments do |t|
+      t.string :user_id
+      t.string :book_id
+
+      t.timestamps
+    end
+  end
+end

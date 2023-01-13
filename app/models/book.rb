991f5063f7ca63480fe4947fc936e9939bfc4bfb
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-13 04:29:04 +0000

    [add]アソシエーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user, optional: true
   has_many :favorite, dependent: :destroy
+  has_many :book_comment, dependent: :destroy
   validates :title,presence: true
   validates :body,presence: true,length:{maximum:200}
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   # belongs_to :books
   has_many :books, dependent: :destroy
   has_many :favorite, dependent: :destroy
+  has_many :book_comment, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, presence: true, length: {in:2..20}, uniqueness: true

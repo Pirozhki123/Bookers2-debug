e96e67c22aadb1e41d2e21b81445981ba2f92fd2
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-02-03 08:44:20 +0000

    [中断]コメントの非同期化中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
+    book_comment.book_id = @book.id
     book_comment.save
-    redirect_to book_path(book)
+    render 'books/create'
+    # redirect_to book_path(book)
   end
 
   def destroy
+    # @book = Book.find(params[:book_id])
+    # @book_comment = current_user.book_comments.new(book_comment_params)
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
   end
 
   private

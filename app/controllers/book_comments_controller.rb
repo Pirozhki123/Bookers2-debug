3ae635b7089e8060383140019078463b2cd2750b
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-02-04 02:04:40 +0000

    [add]コメントの非同期通信（問題あり）

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = @book.id
-    book_comment.save
-    render 'books/create'
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    # render 'book_comments/create'
     # redirect_to book_path(book)
   end
 
   def destroy
-    # @book = Book.find(params[:book_id])
-    # @book_comment = current_user.book_comments.new(book_comment_params)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.find(params[:id])
     BookComment.find(params[:id]).destroy
     # redirect_to book_path(params[:book_id])
   end
